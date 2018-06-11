cb5fb1753dc1550d8b821d5e55d12e2f24284611
STUDENT.JAC.Lara <lara@lara.com>
2018-06-11 11:13:40 -0400

    Day 5
    First upload

---
+++ Website/ADONET_Demo/ADONET_Demo/SupliersSearch.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SupliersSearch.aspx.cs" Inherits="ADONET_Demo.SupliersSearch" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-small;
+        }
+        .auto-style2 {
+            width: 100%;
+        }
+        .auto-style3 {
+            width: 142px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    &nbsp;<br />
+        <br />
+        <br />
+        <table class="auto-style2">
+            <tr>
+                <td class="auto-style3">Supplier ID</td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="tbSupplier" runat="server" TextMode="Number" Width="250px"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:Button ID="btnSearch" runat="server" OnClick="btnSearch_Click" Text="Search" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">&nbsp;</td>
+                <td class="auto-style3">
+                    <asp:ListBox ID="lbSuppliers" runat="server" Width="250px"></asp:ListBox>
+                </td>
+                <td>
+                    <asp:DropDownList ID="ddlSuppliers" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlSuppliers_SelectedIndexChanged" Width="250px">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">&nbsp;</td>
+                <td class="auto-style3">
+                    <asp:DetailsView ID="dvSupplier" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" Height="50px" Width="125px">
+                        <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+                        <CommandRowStyle BackColor="#E2DED6" Font-Bold="True" />
+                        <EditRowStyle BackColor="#999999" />
+                        <FieldHeaderStyle BackColor="#E9ECF1" Font-Bold="True" />
+                        <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                        <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                        <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                        <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+                    </asp:DetailsView>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style3">&nbsp;</td>
+                <td class="auto-style3">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style3">&nbsp;</td>
+                <td class="auto-style3">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="lblConnection" runat="server" CssClass="auto-style1" ForeColor="#009900"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Website/ADONET_Demo/ADONET_Demo/SupliersSearch.aspx.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//Step 1 - ADD NAME SPACES TO USE
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration; //this provides access to web.config
+namespace ADONET_Demo
+{
+    public partial class SupliersSearch : System.Web.UI.Page
+    {
+        private string ConString = ConfigurationManager.ConnectionStrings
+            ["NORTHWNDConnectionString_ADO"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+                return;
+            if (!Page.IsPostBack)
+            {
+
+            }
+            //Populate my drop down list 
+            SqlConnection con = new SqlConnection(ConString);
+
+            try
+            {
+                string sqlQuery = "select * from Suppliers";
+                //lblConnection.Text = "Connection established... :)";
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                ListItem selectItem = new ListItem();
+                selectItem.Text = "Please select a supplier";
+                selectItem.Value = "";
+                ddlSuppliers.Items.Add(selectItem);
+                while (reader.Read())
+                {
+                    ListItem newItem = new ListItem();
+                    newItem.Text = reader["SupplierID"]+"."+reader["CompanyName"];
+                    newItem.Value = reader["SupplierID"].ToString();
+                    ddlSuppliers.Items.Add(newItem);
+                    
+                }
+            }
+            catch (Exception er)
+            {
+                lblConnection.ForeColor = System.Drawing.Color.Red;
+                lblConnection.Text = "Connection error... :(" + er.Message;
+            }
+            finally
+            {
+                con.Close();
+                //lblConnection.Text += "...Now the connection is closed..";
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConString);
+
+            try
+            {
+                lbSuppliers.Items.Clear();
+                string sqlQuery = "select * from Suppliers where SupplierID =" + tbSupplier.Text;
+                //lblConnection.Text = "Connection established... :)";
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                SqlDataReader reader;
+                con.Open();
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    lbSuppliers.Items.Add("ID: " + reader["SupplierID"]);
+                    lbSuppliers.Items.Add("Name: " + reader["CompanyName"]);
+                    lbSuppliers.Items.Add("Address: " + reader["Address"]);
+                    lbSuppliers.Items.Add("Phone: " + reader["Phone"]);
+                }
+                else
+                    lbSuppliers.Items.Add("No Record found");
+            }
+            catch (Exception er)
+            {
+                lblConnection.ForeColor = System.Drawing.Color.Red;
+                lblConnection.Text = "Connection error... :("+er.Message;
+            }
+            finally
+            {
+                con.Close();
+                //lblConnection.Text += "...Now the connection is closed..";
+            }
+        }
+
+        protected void ddlSuppliers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            SqlConnection con = new SqlConnection(ConString);
+
+            try
+            {
+                if (ddlSuppliers.SelectedIndex != 0)
+                {
+                    string sqlQuery = "select * from Suppliers where SupplierID =" + ddlSuppliers.SelectedValue;
+
+                    SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    dvSupplier.DataSource = reader;
+                    dvSupplier.DataBind();
+                }
+                else
+                {
+                    dvSupplier.DataSource = null;
+                    dvSupplier.DataBind();
+                }
+                
+            }
+            catch (Exception er)
+            {
+                lblConnection.ForeColor = System.Drawing.Color.Red;
+                lblConnection.Text = "Connection error... :(" + er.Message;
+            }
+            finally
+            {
+                con.Close();
+                //lblConnection.Text += "...Now the connection is closed..";
+            }
+        }
+    }
+}
+/**
+ * 
+            SqlConnection con = new SqlConnection(ConString);
+            try
+            {
+                con.Open();
+                lblConnection.Text = "Connection established... :)";
+            }
+            catch (Exception er)
+            {
+                lblConnection.ForeColor = System.Drawing.Color.Red;
+                lblConnection.Text = "Connection error... :(";
+            }
+            finally
+            {
+                con.Close();
+                lblConnection.Text += "...Now the connection is closed..";
+            }
+*/
